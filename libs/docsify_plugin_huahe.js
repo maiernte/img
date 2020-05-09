c4fcb35abc37e79a24e5c17bf1251750c9ed0f73
maiernte <mai.yongfeng@gmail.com>
2020-05-09 17:24:52 +0200

    clean math package

--- libs/docsify_plugin_huahe.js
+++ libs/docsify_plugin_huahe.js
@@ -1,40 +1,26 @@
-var dictYiXue = {}
-var settingYixue = {
-    "字体大小": 3, // 字体大小 1~9 数字越大字体越大
-    "水土共长生": true, // 是否水土共长生
-    "华鹤八字正偏财": true,  // 是否使用华鹤偏财
-    "开启短名": false, // 是否使用短名，如“偏财”->“才”
-    "Katex": true
-};
-
 (function () {
-  function yixue(math){
-    var pattern = /(?<express>\\[a-zA-Z0-9]+)\{/
-    var macro = math.match(pattern)
-    var key = macro ? macro.groups["express"] : ""
-    var func = dictYiXue[key]
-    if(func){
-      return func(math)
-    }else{
-      return math
-    }
-  }
-
   function install(hook) {
     hook.init(function() {
       // 初始化完成后调用，只调用一次，没有参数。
+      /* if(window.screen.width < 400) {
+        settingYixue.set.shortname = true;
+      } */
+      console.log('initall.....')
    });
 
     hook.doneEach(function() {
+      console.log('doneEach.....')
       // 每次路由切换时数据全部加载完成后调用，没有参数。
       // ...
       renderMathInElement(document.body, {
-        preProcess: yixue,
+        preProcess: window.yixueRender,
         delimiters: [
           {left: "$$", right: "$$", display: true},
           {left: "\\[", right: "\\]", display: true},
           {left: "$", right: "$", display: false},
-          {left: "\\(", right: "\\)", display: false}
+          {left: "\\(", right: "\\)", display: false},
+          {left: "¥¥", right: "¥¥", display: true},
+          {left: "¥", right: "¥", display: false},
         ]
       });
     });
@@ -84,3 +70,87 @@ var settingYixue = {
     }
   ]
 }; */
+
+
+/* $(document)
+    .ready(function() {
+      $('.ui.form')
+        .form({
+          fields: {
+            email: {
+              identifier  : 'email',
+              rules: [
+                {
+                  type   : 'empty',
+                  prompt : 'Please enter your e-mail'
+                },
+                {
+                  type   : 'email',
+                  prompt : 'Please enter a valid e-mail'
+                }
+              ]
+            },
+            password: {
+              identifier  : 'password',
+              rules: [
+                {
+                  type   : 'empty',
+                  prompt : 'Please enter your password'
+                },
+                {
+                  type   : 'length[6]',
+                  prompt : 'Your password must be at least 6 characters'
+                }
+              ]
+            }
+          }
+        })
+      ;
+    })
+  ;
+
+  function myFunction() {
+    console.log('login.......')
+    var element = document.getElementById("loginForm");
+    element.parentNode.removeChild(element);
+    window.$docsify.loadSidebar = "_sidebar.md"
+    window.$docsify.loadNavbar = "_navbar.md"
+    console.log(window.$docsify)
+} */
+
+
+/* <div class="ui middle aligned center aligned grid" id="loginForm" 
+    style="background-color: #DADADA;height: 100%;">
+    <div class="column" style="max-width: 450px;">
+      <h2 class="ui teal image header">
+        <img src="./Login Example - Semantic_files/logo.png" class="image">
+        <div class="content">
+          Log-in to your account
+        </div>
+      </h2>
+      <form class="ui large form">
+        <div class="ui stacked segment">
+          <div class="field">
+            <div class="ui left icon input">
+              <i class="user icon"></i>
+              <input type="text" name="email" placeholder="E-mail address">
+            </div>
+          </div>
+          <div class="field">
+            <div class="ui left icon input">
+              <i class="lock icon"></i>
+              <input type="password" name="password" placeholder="Password">
+            </div>
+          </div>
+          <div class="ui fluid large teal submit button" onclick="myFunction()">Login</div>
+        </div>
+  
+        <div class="ui error message"></div>
+  
+      </form>
+  
+      <div class="ui message">
+        New to us? <a href="https://semantic-ui.com/examples/login.html#">Sign Up</a>
+      </div>
+    </div>
+  </div>  */
